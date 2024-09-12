cf2ef034d3878d1f8930699eebc1916b41a00dcf
ardi-b <thesonicman2@yahoo.co.id>
2024-09-12 14:42:39 +0700

    css bottom_navbar

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import React, { Component } from "react";
+import styles from "../page.module.css";
 
 class Footer extends Component {
   render() {
     return (
-      <div style={{ position: "absolute", bottom: 0, width:"100%" }} className="bg-gray-100">
+      <div className={styles.bottom_navbar}>
         <div className="bg-gray-100 container mx-auto px-6 pt-10 pb-6">
             © Oracle Corp. All rights reserved.
         </div>
